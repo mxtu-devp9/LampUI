f7d9116bb81db72d2813a3f615f0f292d63b820e
mxtu-devp9 <165764310+mxtu-devp9@users.noreply.github.com>
2024-11-19 21:30:16 +0530

    Add files via upload

---
+++ LampUISwiftUI-main/LampUI/LampUI/LampUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LampUIApp.swift
+//  LampUI
+//
+//  Created by Miten Mistry on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LampUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
